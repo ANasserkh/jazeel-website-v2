891afd00c393af675125ae42c4a4389c9cdf0816
Nasser <nasse.kh.kh@gmail.com>
2026-05-04 16:11:28 +0300

    fix: change urls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,36 @@
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	app: {
-		head: {
-			script: [
-				{
-					src: "https://platform-api.sharethis.com/js/sharethis.js",
-					async: true,
-					defer: true,
-				},
-			],
-		},
-	},
-	runtimeConfig: {
-		public: {
-			apiBase: "https://localhost:7012/website/api",
-			apiMaster: "https://localhost:7049/api/website",
-		},
-	},
+  compatibilityDate: "2025-07-15",
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://platform-api.sharethis.com/js/sharethis.js",
+          async: true,
+          defer: true,
+        },
+      ],
+    },
+  },
+  runtimeConfig: {
+    // public: {
+    //   apiBase: "https://localhost:7012/website/api",
+    //   apiMaster: "https://localhost:7049/api/website",
+    // },
 
-	devtools: { enabled: true },
-	css: ["./app/assets/css/main.css"],
-	postcss: {
-		plugins: {
-			tailwindcss: {},
-			autoprefixer: {},
-		},
-	},
+    public: {
+      apiBase: "https://ngo.jazeel.net.sa/website/api",
+      apiMaster: "https://master.jazeel.net.sa/api/website",
+    },
+  },
 
-	modules: ["@vueuse/nuxt"],
+  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: ["@vueuse/nuxt"],
 });
